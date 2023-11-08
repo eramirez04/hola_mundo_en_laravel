485e18505406baa3e553dce6284e68ea5dde63fd
eramirez04 <emerssonramirez510@gmail.com>
2023-11-08 13:46:50 -0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        @vite('resources/css/app.css')
+        <!-- Styles -->
+        <style>
+
+        </style>
+    </head>
+    <body class="antialiased content-center">
+       <h1 class="text-3xl font-bold underline bg-red-50 text-yellow-600">HOlA MUNDO</h1>
+    </body>
+</html>
